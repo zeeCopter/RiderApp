0c714cd14bf4366e3bdb3669364be1870101fc30
Zeeshan Ahmed <zeeshan.ahmed22805@gmail.com>
2018-10-05 09:32:01 -0700

    Last Commit...

---
+++ app/src/main/java/com/example/ejaaz/riderapp/DocumentStatusActivity.java
@@ -0,0 +1,13 @@
+package com.example.ejaaz.riderapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DocumentStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_status);
+    }
+}
