c047ad584f89549f4d21eb65fa427878bfece8bb
zishuCopter <zeeshan.ahmed22805@gmail.com>
2018-10-06 17:37:07 +0500

    Some More Screens....

---
+++ app/src/main/java/com/example/ejaaz/riderapp/BookTraningSlotTwoActicvity.java
@@ -0,0 +1,13 @@
+package com.example.ejaaz.riderapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BookTraningSlotTwoActicvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_traning_slot_two);
+    }
+}
